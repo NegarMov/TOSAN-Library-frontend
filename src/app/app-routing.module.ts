7611912be42ed7b0d26e3ca9db48031aa2f48c94
Movaghatian Negar <n.movaghatian@tosan.com>
2021-04-07 17:43:54 +0430

    start routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookInfoComponent } from './book-info/book-info.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RequestsComponent } from './requests/requests.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'log-in', component: LoginFormComponent },
+  { path: 'sign-up', component: LoginFormComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'requests', component: RequestsComponent },
+  { path: 'book/:bookTitle', component: BookInfoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
