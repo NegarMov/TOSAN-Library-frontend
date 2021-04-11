4da3b1a4e3e2d009be5b074343197a405eba4d8e
Movaghatian Negar <n.movaghatian@tosan.com>
2021-04-11 16:57:59 +0430

    modified book info

--- src/app/book-info/book-info.component.ts
+++ src/app/book-info/book-info.component.ts
@@ -7,14 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookInfoComponent implements OnInit {
 
+  access: string = "admin"; //change later
+
   bookTitle: string = "First Book";
   bookAuthor: string = "Maxeu";
   bookPublisher: string = "The Other";
   bookSummary: string = "This is a book about books? This author writes about books, in fact writes everything except the book itself! You know, it gets a little bit recursive if she did the thing. Wanna be on this journey with her? Read this book!";
   rating: number = 3;
+  tags = ["comedy", "social", "sci-fi"];
   saveMessage: string = "Add to read later";
   requestSent: boolean = false;
-  userRating: number = 0;
+  userRating: number = 0; //for user
+  lentTo: string = "@username"; //for admin (in case it's empty, the book is available)
+  startDate: Date = new Date(2020, 7, 12, 13, 54, 7); //for admin
+  dueDate: Date = new Date(2020, 7, 19, 13, 54, 7); //for admin
 
   onclickSave() {
     if (this.saveMessage == "Add to read later") {
